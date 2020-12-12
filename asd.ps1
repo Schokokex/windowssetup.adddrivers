dece50af4a52d2559fcf40ed95ec21e2bd8e8dc2
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-12-12 02:59:07 +0100

    Create asd.ps1

---
+++ asd.ps1
@@ -0,0 +1,23 @@
+
+
+
+$isoDrive = "D:"
+$targetDrive = "H:"
+
+echo remove install.esd-readonly and install.wim-readonly if neccessary
+
+
+
+mkdir $targetDrive\drivers
+Export-WindowsDriver -Online -Destination $targetDrive\drivers
+
+cp -r $isoDrive\* $targetDrive # install.esd can be skipped
+
+attrib -R $targetDrive\sources\install.esd
+Export-WindowsImage -SourceImagePath $targetDrive\sources\install.esd -SourceIndex 4 -DestinationImagePath $targetDrive\sources\install.wim -CompressionType max -CheckIntegrity 
+rm $targetDrive\sources\install.esd
+
+mkdir $targetDrive\wim
+Mount-WindowsImage -ImagePath $targetDrive\sources\install.wim -Index 1 -Path $targetDrive\wim
+Add-WindowsDriver -Path $targetDrive\wim -Driver $targetDrive\drivers -Recurse
+Dismount-WindowsImage -Path $targetDrive\wim  -Save
